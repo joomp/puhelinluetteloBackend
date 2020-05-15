bbe250fe8acb8669efe29402a2107dd2d3bb70df
Joona Mäkipernaa <joona.makipernaa@tuni.fi>
2020-05-15 19:46:29 +0300

    uibuild

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # puhelinluetteloBackend
+Link to app
+https://cryptic-lake-32405.herokuapp.com/
---
+++ build/precache-manifest.f2df179d32e75df6ac3ebc2df6efa8dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1184c41ac7ac87474d737cf9f530b700",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dc64c929542e02af69e",
+    "url": "/static/css/main.b6df74cd.chunk.css"
+  },
+  {
+    "revision": "64d0a60b036041c8f0d5",
+    "url": "/static/js/2.633a1fc2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.633a1fc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dc64c929542e02af69e",
+    "url": "/static/js/main.f674e8ac.chunk.js"
+  },
+  {
+    "revision": "d6d4691db26ca793a81a",
+    "url": "/static/js/runtime-main.4f3ee9ca.js"
+  }
+]);
